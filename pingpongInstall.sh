5e9e581e92f348691321abb37d9654a4b6770feb
Chen yangu <975991248@qq.com>
2024-05-20 23:33:18 +0800

    Update pingpongInstall.sh

--- pingpongInstall.sh
+++ pingpongInstall.sh
@@ -8,4 +8,4 @@ yes | sudo install -m 0755 -d /etc/apt/keyrings
 yes | sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 yes | sudo chmod a+r /etc/apt/keyrings/docker.asc
 yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-chmod +x ./PINGPONG && ./PINGPONG --key 1f9ebc8b04b80a7b9b46f1e12f71a9828da078bbea7785a359889bf72347ea1a
+chmod +x ./PINGPONG
