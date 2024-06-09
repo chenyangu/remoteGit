7b8e6d6c6f4f85af30977d306ab3cf989cdc2337
Chen yangu <975991248@qq.com>
2024-06-09 12:29:48 +0800

    Update install19.sh

--- gua/install19.sh
+++ gua/install19.sh
@@ -37,7 +37,7 @@ EOF
 chmod +x /root/qlog.sh
 
 # 检查当前安装的 Go 版本
-if go version | grep -q "go1.20.1[1-4]"; then
+if go version | grep -q "go1.22.1[1-4]"; then
   echo "Go已经安装..."
 else
   echo "安装Go..."
@@ -45,9 +45,9 @@ else
   # 检查系统架构
   ARCH=$(uname -m)
   if [[ "$ARCH" == "x86_64" ]]; then
-    GO_URL="http://49.13.194.189:8008/go1.20.14.linux-amd64.tar.gz"
+    GO_URL="https://go.dev/dl/go1.22.1.linux-amd64.tar.gz"
   elif [[ "$ARCH" == "aarch64" ]]; then
-    GO_URL="http://49.13.194.189:8008/go1.20.14.linux-arm64.tar.gz"
+    GO_URL="https://go.dev/dl/go1.22.1.linux-arm64.tar.gz"
   else
     echo "不支持的架构: $ARCH"
     exit 1
