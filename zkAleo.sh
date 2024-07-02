ac46a676403c63728aceb21be1e954df76d460ca
Chen yangu <975991248@qq.com>
2024-07-02 17:17:28 +0800

    Update zkAleo.sh

--- zkAleo.sh
+++ zkAleo.sh
@@ -21,8 +21,28 @@ fi
 
 cd $DIR
 
-echo "开始下载zk aleo文件"
-wget -c https://github.com/zkrush/aleo-pool-client/releases/download/v1.5-testnet-beta/aleo-pool-prover
+LOCAL_FILE = $DIR/aleo-pool-prover
+# 检查本地文件是否存在
+if [ -f "$LOCAL_FILE" ]; then
+    echo "本地文件 $LOCAL_FILE 已存在。"
+
+    # 获取本地文件的MD5哈希值
+    LOCAL_HASH=$(md5sum "$LOCAL_FILE" | awk '{ print $1 }')
+
+    # 获取服务器文件的MD5哈希值
+    SERVER_HASH=$(wget -qO- "$URL" | md5sum | awk '{ print $1 }')
+
+    if [ "$LOCAL_HASH" == "$SERVER_HASH" ]; then
+        echo "本地文件与服务器文件一致，不需要下载。"
+    else
+        echo "本地文件与服务器文件不一致，重新下载。"
+        wget -c https://github.com/zkrush/aleo-pool-client/releases/download/v1.5-testnet-beta/aleo-pool-prover
+    fi
+else
+    echo "本地文件 $LOCAL_FILE 不存在，正在下载..."
+    wget -O "$LOCAL_FILE" "$URL"
+fi
+
 echo "下载完成"
 chmod +x aleo-pool-prover
 cat <<EOF > /etc/systemd/system/aleo-pool-prover.service
