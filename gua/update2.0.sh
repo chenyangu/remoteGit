e53e7fbb4c71cb5874251cc556eeca5952f0925d
Chen yangu <975991248@qq.com>
2024-10-13 11:43:03 +0800

    Create update2.0.sh

---
+++ gua/update2.0.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    release_os="linux"
+    if [[ $(uname -m) == "aarch64"* ]]; then
+        release_arch="arm64"
+    else
+        release_arch="amd64"
+    fi
+else
+    release_os="darwin"
+    release_arch="arm64"
+fi
+
+fetch() {
+    files=$(curl https://releases.quilibrium.com/release | grep $release_os-$release_arch)
+    new_release=false
+
+    for file in $files; do
+        version=$(echo "$file" | cut -d '-' -f 2)
+        if ! test -f "./$file"; then
+            curl "https://releases.quilibrium.com/$file" > "$file"
+            new_release=true
+        fi
+    done
+}
+
+fetch
+
+chmod +x node-2.0*
