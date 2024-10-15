2f47f0ae38331c20dee3f92d318ed6cfb900ad2a
Chen yangu <975991248@qq.com>
2024-10-15 11:20:31 +0800

    Update update2.0.sh

--- gua/update2.0.sh
+++ gua/update2.0.sh
@@ -25,6 +25,21 @@ fetch() {
     done
 }
 
+fetchClient() {
+    files=$(curl https://releases.quilibrium.com/qclient-release | grep $release_os-$release_arch)
+    new_release=false
+
+    for file in $files; do
+        version=$(echo "$file" | cut -d '-' -f 2)
+        if ! test -f "./$file"; then
+            curl "https://releases.quilibrium.com/$file" > "$file"
+            new_release=true
+        fi
+    done
+}
+
 fetch
+fetchClient
 
 chmod +x node-2.0*
+chmod +x qclient-2.0*
