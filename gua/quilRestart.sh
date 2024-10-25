2a604ec7e449c3fcbbaf74acd53cbfcdeacd915b
Chen yangu <975991248@qq.com>
2024-10-25 13:26:15 +0800

    Update quilRestart.sh

--- gua/quilRestart.sh
+++ gua/quilRestart.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+binary="node-2.0.1-$release_os-$release_arch"
 # 循环执行，直到手动停止
 while true; do
   echo "Starting ./node-2.0.1-linux-amd64..."
   
   # 启动二进制文件，使用 & 使其在后台运行
-  ./node-2.0.1-linux-amd64 
+  ./$binary
 
   # 等待20分钟
   sleep 1200
   # 查找并杀死所有 node-2.0.1-linux-amd64 进程
-  pkill -f "./node-2.0.1-linux-amd64"
+  pkill -f "./$binary"
 
   echo "All node-2.0.1-linux-amd64 processes have been terminated."
 done
